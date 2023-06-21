0b20999e9af0b247c201a34ab83fbcd9b7696d9d
Sanya Midha <sanyamidha.10@gmail.com>
2023-06-21 10:23:23 +0530

    Login feature

--- blog_main/views.py
+++ blog_main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect, render
 from blogs.models import Category, Blog
 from .forms import RegistrationForm
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
 
 def home(request):
     featured_posts = Blog.objects.filter(is_featured=True, status='Published').order_by('updated_at')
@@ -17,7 +18,7 @@ def register(request):
         form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('register')
+            return redirect('login')
     else:
         form = RegistrationForm()
     context = {
@@ -26,6 +27,15 @@ def register(request):
     return render(request, 'register.html', context)
 
 def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+            return redirect('home')
     form = AuthenticationForm()
     context = {
         'form': form,
