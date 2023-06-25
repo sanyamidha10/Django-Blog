558b9bbd65a46f2f338681fd40a8408ac2e1c1f1
Sanya Midha <sanyamidha.10@gmail.com>
2023-06-25 13:52:25 +0530

    Dashboard design

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from blogs.models import Blog, Category
 
 # Create your views here.
 def dashboard(request):
-    return render(request, 'dashboard/dashboards.html')
+    category_count = Category.objects.all().count()
+    blogs_count = Blog.objects.all().count()
+
+    context = {
+        'category_count': category_count,
+        'blogs_count': blogs_count,
+    }
+
+    return render(request, 'dashboard/dashboards.html', context)
