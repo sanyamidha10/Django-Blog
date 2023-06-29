9930c4994e3ec313f47af95c53eee2c32e1b1d9c
Sanya Midha <sanyamidha.10@gmail.com>
2023-06-29 11:52:29 +0530

    Edit category

--- dashboards/urls.py
+++ dashboards/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('categories/', views.categories, name='categories'),
     path('categories/add/', views.add_category, name='add_category'),
-
+    path('categories/edit/<int:pk>/', views.edit_category, name='edit_category'),
 
 ]
--- dashboards/views.py
+++ dashboards/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from blogs.models import Blog, Category
 from django .contrib.auth.decorators import login_required
 from . forms import CategoryForm
@@ -32,3 +32,17 @@ def add_category(request):
     }
     return render(request, 'dashboard/add_category.html', context)
 
+def edit_category(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    if request.method == 'POST':
+        form = CategoryForm(request.POST, instance = category)
+        if form.is_valid():
+            form.save()
+            return redirect('categories')
+    else:
+        form = CategoryForm(instance=category)
+    context = {
+        'form': form,
+        'category': category,
+    }
+    return render(request, 'dashboard/edit_category.html', context)
