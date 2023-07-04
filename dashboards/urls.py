e47699375965117f05d2a93e60bfb4b891ec195e
Sanya Midha <sanyamidha.10@gmail.com>
2023-07-04 16:35:39 +0530

    Edit Post

--- dashboards/urls.py
+++ dashboards/urls.py
@@ -12,5 +12,7 @@ urlpatterns = [
 
     # Posts crud
     path('posts/', views.posts, name='posts'),
-    path('posts/add/', views.add_post, name='add_post')
+    path('posts/add/', views.add_post, name='add_post'),
+    path('posts/edit/<int:pk>/', views.edit_post, name='edit_post'),
+    
 ]
